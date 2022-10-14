58746ffc6a6ac2832882dd9f44210d68d95c434a
Nikita Sokolov UA <nsokolov0309@gmail.com>
2022-10-14 17:21:44 +0300

    add formik && yup to the project

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,73 +1,47 @@
-import React, { Component } from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
-import { FormContainer, Input, Label, LabelName, SubmitButton } from "./ContactForm.styled";
+import { Formik, Form, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+import { FormContainer, Input, SubmitButton } from './ContactForm.styled';
 
-class ContactForm extends Component {
-    state= {
-        name: '',
-        number: '',
-    }
+const schema = yup.object().shape({
+  name: yup.string().required(),
+  number: yup.string().required().min(7, 'минимум 7 символов').max(20),
+});
 
-    handleChange = e => {
-        const {name, value} = e.currentTarget
-        this.setState({[name]: value})
-    }
+const ContactForm = ({ onSubmit }) => {
+  const InitialValues = {
+    name: '',
+    number: '',
+  };
 
-    handleSubmitForm = e => {
-        e.preventDefault()
+  const onSubmitForm = ({ name, number }, { resetForm }) => {
+    onSubmit(name, number);
+    resetForm();
+  };
 
-        const { name, number } = this.state
-        
-        this.props.onSubmit(name, number)
+  return (
+    <FormContainer>
+      <Formik
+        initialValues={InitialValues}
+        validationSchema={schema}
+        onSubmit={onSubmitForm}
+      >
+        <Form>
+          <Input type="text" name="name" />
+          <ErrorMessage name="name" />
+          <Input type="text" name="number" />
+          <ErrorMessage name="number" />
 
-        this.reset()
-    }
-
-    reset = () => {
-    this.setState({
-      name: '',
-      number: '',
-    })
-    }
-
-    render() {
-        const { name, number } = this.state
-
-      return <FormContainer>
-      <form onSubmit={this.handleSubmitForm}>
-        <Label >
-          <LabelName>Name</LabelName>
-          <Input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            value={name}
-            onChange={this.handleChange}
-          />
-        </Label>
-        <Label >
-          <LabelName>Telephone</LabelName>
-          <Input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            value={number}
-            required
-            onChange={this.handleChange}
-          />
-        </Label>
-
-        <SubmitButton type="submit">Save</SubmitButton>
-        </form>
-        </FormContainer>
-    }
-}
+          <SubmitButton type="submit">Save</SubmitButton>
+        </Form>
+      </Formik>
+    </FormContainer>
+  );
+};
 
 ContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
-}
+};
 
-export default ContactForm
+export default ContactForm;
--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -1,68 +1,67 @@
-import styled from "@emotion/styled"
+import styled from '@emotion/styled';
+import { Field } from 'formik';
 
 export const FormContainer = styled.div`
-    width: 400px;
-    padding: 20px;
-    margin-left: auto;
-    margin-right: auto;
-`
+  width: 400px;
+  padding: 20px;
+  margin-left: auto;
+  margin-right: auto;
+`;
 
 export const Form = styled.form`
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-`
-export const Label = styled.label`
-     
-    
-    
-`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+export const Label = styled.label``;
 export const LabelName = styled.span`
-margin-left: 40px;
-margin-bottom: 5px;
-display: block;
-    color: #757575;
-    letter-spacing: 0.03em;
-`
+  margin-left: 40px;
+  margin-bottom: 5px;
+  display: block;
+  color: #757575;
+  letter-spacing: 0.03em;
+`;
 
-export const Input = styled.input`
-   margin-left: 40px;
-   padding-left: 20px;
-    width: 80%;
-    height: 40px;
-    border: 1px solid rgba(33, 33, 33, 0.2);
-    border-radius: 4px;
-    font-size: 20px;
-    letter-spacing: 0.06em;
-    transition: border 250ms cubic-bezier(0.4, 0, 0.2, 1);
+export const Input = styled(Field)`
+  margin-left: 40px;
+  padding-left: 20px;
+  width: 80%;
+  height: 40px;
+  border: 1px solid rgba(33, 33, 33, 0.2);
+  border-radius: 4px;
+  font-size: 20px;
+  letter-spacing: 0.06em;
+  transition: border 250ms cubic-bezier(0.4, 0, 0.2, 1);
 
-    :focus-within{
-        border: 1px solid #2196F3;
-    }
-    :focus-visible{
-        border: 1px solid #2196F3;
-        outline: none;
-    }
+  :focus-within {
+    border: 1px solid #2196f3;
+  }
+  :focus-visible {
+    border: 1px solid #2196f3;
+    outline: none;
+  }
 
-    :first-of-type{
-        margin-bottom: 40px;
-    }
-`
+  :first-of-type {
+    margin-bottom: 40px;
+  }
+`;
 
 export const SubmitButton = styled.button`
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    
-    width: 200px;
-    height: 50px;
-    border: none;
-    border-radius: 5px;
-    color: white;
-    background-color: #2F4F4F;
-    cursor: pointer;
-    transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
-    :hover, :focus{
-        background-color: #2196F3;
-    }
-`
+  display: block;
+  margin-top: 20px;
+  margin-left: auto;
+  margin-right: auto;
+
+  width: 200px;
+  height: 50px;
+  border: none;
+  border-radius: 5px;
+  color: white;
+  background-color: #2f4f4f;
+  cursor: pointer;
+  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  :hover,
+  :focus {
+    background-color: #2196f3;
+  }
+`;
